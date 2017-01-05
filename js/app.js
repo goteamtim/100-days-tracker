aae181a7168765e249451b7608438f5206f3b3f7
Tim Harshman <goteamtim+git@gmail.com>
2017-01-04 21:01:46 -0800

    Last updated displayed on the screen successfully

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var app = angular.module('100DaysTrackerApp', []);
 
-app.controller('GitHubController', function ($scope, $http) {
-  $scope.gitHubObject = {},
+app.controller('GitHubController', ['$scope','$http',function ($scope, $http) {
+  $scope.gitHubObject,
+  $scope.hasGitHubData = true,
   $scope.username = "",
   $scope.repo = "";
   $scope.getUserInfo = function() {
@@ -14,10 +15,10 @@ app.controller('GitHubController', function ($scope, $http) {
       //response.data will be only the data if you ever want that
       $scope.gitHubObject = response;
       console.log("success with the url");
-      console.log(response);
+      console.log(response.data.updated_at);
     }, function errorCallback(response) {
       //Show the user something that there is an issue.  Also maybe try and setup something so I log this?
     });
   };
 
-});
+}]);

ac22f728c959b87418e1c5f863f2657e888702f3
Tim Harshman <goteamtim+git@gmail.com>
2017-01-04 21:43:38 -0800

    Cleaned up the header and the file structure

---
+++ app/controllers/Github/Controller.js
@@ -0,0 +1,22 @@
+app.controller('GitHubController', ['$scope','$http',function ($scope, $http) {
+  $scope.gitHubObject,
+  $scope.hasGitHubData = true,
+  $scope.username = "",
+  $scope.repo = "";
+  $scope.getUserInfo = function() {
+    let apiUrl = 'https://api.github.com/repos/' + $scope.username + '/' + $scope.repo;
+    console.log(apiUrl);
+    $http({
+      method: 'GET',
+      url: apiUrl
+    }).then(function successCallback(response) {
+      //response.data will be only the data if you ever want that
+      $scope.gitHubObject = response;
+      console.log("success with the url");
+      console.log(response.data.updated_at);
+    }, function errorCallback(response) {
+      //Show the user something that there is an issue.  Also maybe try and setup something so I log this?
+    });
+  };
+
+}]);

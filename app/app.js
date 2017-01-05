209d6f6bc4497dd05b36409a57034ff7fa57af64
Tim Harshman <goteamtim+git@gmail.com>
2017-01-05 09:54:33 -0800

    Adding functionality

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ app.controller('GitHubController', ['$scope','$http',function ($scope, $http) {
   $scope.gitHubObject,
   $scope.hasGitHubData = true,
   $scope.username = "",
-  $scope.repo = "";
+  $scope.repo = "",
+  $scope.firstTimeUser = checkUserStatus();
   $scope.getUserInfo = function () {
     let apiUrl = 'https://api.github.com/repos/' + $scope.username + '/' + $scope.repo;
     console.log(apiUrl);
@@ -14,7 +15,7 @@ app.controller('GitHubController', ['$scope','$http',function ($scope, $http) {
       url: apiUrl
     }).then(function successCallback(response) {
       //response.data will be only the data if you ever want that
-      $scope.hasGitHubData = true;
+      //$scope.hasGitHubData = true;
       $scope.gitHubObject = response;
       if(activeToday(response.data.updated_at)){
         console.log("You have finished for today!")
@@ -24,23 +25,42 @@ app.controller('GitHubController', ['$scope','$http',function ($scope, $http) {
       console.log("success with the url");
       console.log(response.data.updated_at);
       
+      $scope.$apply(function(){
+        toggleState($scope.hasGitHubData)
+      })
     }, function errorCallback(response) {
       //Show the user something that there is an issue.  Also maybe try and setup something so I log this?
     });
   };
+
+  function checkUserStatus(){
+    if(localStorage.getItem("lastVisitDate")  === null){
+      return true;
+  }else{
+    return false;
+  }
+  }
 
   function activeToday(lastDate){
     var lastDevDate = new Date(lastDate),
     todayDevDate = new Date(),
     timeDifference = todayDevDate.getTime() - lastDevDate.getTime(),
     dayDifference = Math.ceil(timeDifference / (1000* 3600 * 24));
-
-    if(dayDifference == -1){
+    console.log(dayDifference)
+    if(dayDifference > 0){
       return false;
     }else{
       return true;
     }
 
+  }
+
+  function toggleState(currentState){
+    if(currentState){
+      currentState = false;
+    }else{
+      currentState = true;
+    }
   }
 
 }]);

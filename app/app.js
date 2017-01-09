6ae726e4a478ecdebcc4c7f2f11c5c2696bada82
Tim Harshman <goteamtim+git@gmail.com>
2017-01-08 16:07:02 -0800

    Fixed showing the correct message

--- app/app.js
+++ app/app.js
@@ -16,19 +16,20 @@ app.controller('GitHubController', ['$scope', '$http', function ($scope, $http) 
       url: apiUrl
     }).then(function successCallback(response) {
       //response.data will be only the data if you ever want that
-      $scope.hasGitHubData = true;
+      $scope.hasGitHubData = !$scope.hasGitHubData;
       $scope.gitHubObject = response;
       if (activeToday(response.data.pushed_at)) {
         $scope.codedToday = "You have finished for today! Have you <a href=\"https:\/\/twitter.com\/intent\/tweet?text=I%20completed%20my%20%23100daysofcode%20challenge%20for%20today!%20I%20worked%20on\">Tweeted</a> yet?";
       } else {
         $scope.codedToday = "you need to code today!";
       }
+      
       //Should do a check here since the first time people visit this will return null
       if (localStorage.getItem($scope.username + "_userObject") != null) {
         //Something is there, need to update it all
         var storedUserData = localStorage.getItem($scope.username + "_userObject");
         storedUserData = JSON.parse(storedUserData);
-        
+
         if (storedUserData.repos.indexOf($scope.repo) == -1) {
           storedUserData.repos.push($scope.repo);
         }
@@ -39,6 +40,7 @@ app.controller('GitHubController', ['$scope', '$http', function ($scope, $http) 
         
       localStorage.setItem($scope.username + "_userObject", JSON.stringify({ 'username': $scope.username, 'repos': [$scope.repo], 'lastUpdated': new Date(), 'lastGithubApiResponse': response }));
       }
+      
     }, function errorCallback(response) {
       //Show the user something that there is an issue.  Also maybe try and setup something so I log this?
     });
